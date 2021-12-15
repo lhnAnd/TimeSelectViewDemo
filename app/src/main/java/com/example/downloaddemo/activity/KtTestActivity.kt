7c7ce887abe11cdf48635010e152d17596de1f50
lhnAnd <luohaonan@haique-tech.com>
2021-12-15 09:22:09 +0800

    add: 添加返回按钮

--- app/src/main/java/com/example/downloaddemo/activity/KtTestActivity.kt
+++ app/src/main/java/com/example/downloaddemo/activity/KtTestActivity.kt
@@ -5,6 +5,7 @@ import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.downloaddemo.R
@@ -19,17 +20,17 @@ class KtTestActivity: Activity() {
         initView()
     }
 
-    fun initView(){
+    private fun initView(){
+        val backButton = findViewById<Button>(R.id.bn_back)
+        backButton.setOnClickListener { finish() }
         val myRecycler = findViewById<RecyclerView>(R.id.my_recycler_view)
         val exampleList = mutableListOf<ItemData>()
-        for (index in 1..10){
+        for (index in 1..100){
             val data = ItemData(""+index,index)
             exampleList.add(data)
         }
-
         myRecycler.adapter = SelectAdapter(this,exampleList)
         myRecycler.layoutManager = LinearLayoutManager(this)
-
     }
 
     companion object{
